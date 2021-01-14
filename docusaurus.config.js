81330ee574412c25f06e2f900c0141bc35909273
Maurice Faber <maurice.faber@redkubes.com>
2021-01-14 14:10:29 +0100

    trying to get fresh index: revert

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ module.exports = {
     },
     algolia: {
       apiKey: '9c75393c761f1434854cef114eb80e64',
-      indexName: `otomi`,
+      indexName: `redkubes_otomi`,
 
       // Optional: see doc section bellow
       contextualSearch: true,

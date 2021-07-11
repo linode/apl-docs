65279764753ca4c0fb04aa729b5cc4b96875462c
srodenhuis <sander.rodenhuis@gmail.com>
2021-07-12 01:07:54 +0200

    redirects fix

--- redirects.js
+++ redirects.js
@@ -9,26 +9,26 @@ module.exports = [
   },
   {
     from: ['/docs/configuring-apps'],
-    to: '/ee/platform-apps',
+    to: '/docs/ee/platform-apps',
   },
   {
     from: ['/docs/configuring-clusters'],
-    to: '/ee/clusters',
+    to: '/docs/ee/clusters',
   },
   {
     from: ['/docs/configuring-services'],
-    to: '/ee/services',
+    to: '/docs/ee/services',
   },
   {
     from: ['/docs/configuring-teams'],
-    to: '/ee/team-apps',
+    to: '/docs/ee/team-apps',
   },
   {
     from: ['/docs/deploy-changes'],
-    to: '/ee/deploy-changes',
+    to: '/docs/ee/deploy-changes',
   },
   {
     from: ['/docs/settings'],
-    to: '/ee/settings',
+    to: '/docs/ee/settings',
   },
 ]

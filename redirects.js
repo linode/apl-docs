dec6bdb6460ed80d1bdc78a3672e140ffd5f78fb
srodenhuis <sander.rodenhuis@gmail.com>
2021-07-12 00:57:42 +0200

    redirects fix 2

--- redirects.js
+++ redirects.js
@@ -4,43 +4,31 @@ module.exports = [
     to: '/docs/lifecycle-management/upgrades',
   },
   {
-    from: ['/docs/configuration'],
-    to: '/docs/installation/configuration',
-  },
-  {
-    from: ['/docs/deployment'],
-    to: '/docs/installation/deployment',
-  },
-  {
     from: ['/docs/prerequisites'],
-    to: '/docs/installation/prerequisites',
-  },
-  {
-    from: ['/docs/setup'],
-    to: '/docs/installation/setup',
+    to: '/installation/prerequisites',
   },
   {
     from: ['/docs/configuring-apps'],
-    to: '/docs/ee/configuring-apps',
+    to: '/ee/configuring-apps',
   },
   {
     from: ['/docs/configuring-clusters'],
-    to: '/docs/ee/configuring-clusters',
+    to: '/ee/configuring-clusters',
   },
   {
     from: ['/docs/configuring-services'],
-    to: '/docs/ee/configuring-services',
+    to: '/ee/configuring-services',
   },
   {
     from: ['/docs/configuring-teams'],
-    to: '/docs/ee/configuring-teams',
+    to: '/ee/configuring-teams',
   },
   {
     from: ['/docs/deploy-changes'],
-    to: '/docs/ee/deploy-changes',
+    to: '/ee/deploy-changes',
   },
   {
     from: ['/docs/settings'],
-    to: '/docs/ee/settings',
+    to: '/ee/settings',
   },
 ]

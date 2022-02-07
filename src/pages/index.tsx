18270114a4170cb12804f19c85ed5a11968f1b03
srodenhuis <sander.rodenhuis@gmail.com>
2022-02-07 14:23:06 +0100

    title change [ci skip]

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ function Home() {
                 src={useBaseUrl("img/otomi-logo.svg")}
               />
               <span className={styles.heroProjectKeywords}>Kubernetes</span>{" "}
-              Applications Configuration Platform{" "}
+              Application Configuration Platform{" "}
               with{" "}
               <span className={styles.heroProjectKeywords}>
                 developer self-service

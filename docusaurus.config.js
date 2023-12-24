92e9ed2dd2af7e573311e7a2e90ea98687f903fc
Sander Rodenhuis <sander.rodenhuis@redkubes.com>
2023-12-24 17:50:04 +0100

    fix: top menu link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -118,7 +118,7 @@ module.exports = {
           position: "left",
         },
         {
-          to: "docs/get-started/installation",
+          to: "docs/get-started/overview",
           label: "Docs",
           position: "left",
         },

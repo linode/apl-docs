43d0064aff6353f597acbe771938d6e8e94ed6d4
Ani Argjiri <aargjiri@akamai.com>
2025-02-05 13:28:25 +0100

    feat: removed smpt config

--- sidebar-docs.js
+++ sidebar-docs.js
@@ -96,7 +96,6 @@ module.exports = {
               "for-ops/console/settings/dns",
               "for-ops/console/settings/ingress",
               "for-ops/console/settings/oidc",
-              "for-ops/console/settings/smtp",
               "for-ops/console/settings/backup",
               "for-ops/console/settings/obj",
             ],
